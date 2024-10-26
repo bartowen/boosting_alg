f4a1914cd97c0c072742fd73741202f87e5c5889
Felipe Browne <153402996+bartowen@users.noreply.github.com>
2024-10-26 18:14:23 +0000

    contar modelos

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,11 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+def contar_modelos(grid):
+    num_models = 1
+
+    for param, values in grid.items():
+        num_models *= len(values)
+
+    return num_models
